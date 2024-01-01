eb70f71fca9bb6eff73bbffa16994bdb77e9c15c
Vikas <vikasdeshmukh63@gmail.com>
2024-01-01 19:26:36 +0530

    header added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
 import AdminSidebar from "../components/AdminSidebar";
+import { BsSearch } from "react-icons/bs";
+import { FaRegBell } from "react-icons/fa";
+import userImg from "../assets/userImg.png";
+
 
 const Dashboard = () => {
   return (
@@ -6,7 +10,15 @@ const Dashboard = () => {
       {/* siderbar */}
       <AdminSidebar />
       {/* main */}
-      <main>fdsfs</main>
+      <main className="dashboard">
+        {/* header  */}
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for Data, Users, Docs" />
+          <FaRegBell />
+          <img src={userImg} alt="User" />
+        </div>
+      </main>
     </div>
   );
 };
