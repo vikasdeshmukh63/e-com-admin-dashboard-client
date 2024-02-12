ac22dcbe3f37ee4f6ab7cc18e76ad1e7b4acfe8e
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2024-02-12 13:52:14 +0530

    all charts completed

--- src/pages/charts/LineCharts.tsx
+++ src/pages/charts/LineCharts.tsx
@@ -19,6 +19,39 @@ const LineCharts = () => {
           />
           <h2>Active Users</h2>
         </section>
+
+        <section>
+          <LineChart
+            data={[40, 60, 244, 100, 143, 120, 41, 47, 50, 56, 32]}
+            label="Products"
+            borderColor="hsl(269,80%,40%)"
+            backgroundColor={"hsla(269,80%,40%,0.4)"}
+            labels={months}
+          />
+          <h2>Total Products (SKU)</h2>
+        </section>
+
+        <section>
+          <LineChart
+            data={[24000, 14400, 24100, 34300, 90000, 20000, 25600, 44700, 99000, 144400, 100000, 120000]}
+            label="Revenue"
+            borderColor="hsl(129,80%,40%)"
+            backgroundColor={"hsla(129,80%,40%,0.4)"}
+            labels={months}
+          />
+          <h2>Total Revenue</h2>
+        </section>
+
+        <section>
+          <LineChart
+            data={[9000, 12000, 12000, 9000, 1000, 5000, 4000, 1200, 1100, 1500, 2000, 5000]}
+            label="Discount"
+            borderColor="hsl(29,80%,40%)"
+            backgroundColor={"hsla(29,80%,40%,0.4)"}
+            labels={months}
+          />
+          <h2>Discount Alloted</h2>
+        </section>
       </main>
     </div>
   );
