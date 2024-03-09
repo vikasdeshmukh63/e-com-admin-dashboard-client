d0292152017bdf20e1137f937ace70c902610c22
vikasdeshmukh63 <vikasdeshmukh63@gmail.com>
2024-03-09 17:54:22 +0530

    coupoun and toss added

--- src/pages/apps/Toss.tsx
+++ src/pages/apps/Toss.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
 
 const Toss = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [angle, setAngle] = useState<number>(0);
 
-export default Toss
+  const flipCoin = () => {
+    if (Math.random() > 0.5) {
+      setAngle((prev) => prev + 180);
+    } else {
+      setAngle((prev) => prev + 360);
+    }
+  };
+
+  return (
+    <div className="adminContainer">
+      <AdminSidebar />
+      <main className="dashboardAppContainer">
+        <h1>Toss</h1>
+        <section>
+          <article className="tossCoin" onClick={flipCoin} style={{ transform: `rotateY(${angle}deg)` }}>
+            <div></div>
+            <div></div>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Toss;
